b92a5910e1f761e70745015df034af5ff590c667
MaxLee18 <manstis@outlook.com.au>
2025-06-14 16:51:34 +1000

    Update layout.tsx

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,8 +1,1 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head />
-      <body>{children}</body>
-    </html>
-  );
-}
+
